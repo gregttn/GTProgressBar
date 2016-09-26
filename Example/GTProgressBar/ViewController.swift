66c56b53c5a27c651b9bd41345dd650bc3ff7dcb
gregttn <gregttn@gmail.com>
2016-09-26 09:03:01 +0100

    Added more example progress bars to the example app.

--- Example/GTProgressBar/ViewController.swift
+++ Example/GTProgressBar/ViewController.swift
@@ -12,18 +12,28 @@ import GTProgressBar
 class ViewController: UIViewController {
 
     @IBOutlet weak var progressBar: GTProgressBar!
+    @IBOutlet weak var orangeProgressBar: GTProgressBar!
+    @IBOutlet weak var redProgressBar: GTProgressBar!
+    @IBOutlet weak var zeroProgressBar: GTProgressBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let labelInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
         // Do any additional setup after loading the view, typically from a nib.
-        progressBar.progress = 0.5
+        progressBar.progress = 1
         progressBar.barBorderColor = UIColor(red:0.35, green:0.80, blue:0.36, alpha:1.0)
         progressBar.barFillColor = UIColor(red:0.35, green:0.80, blue:0.36, alpha:1.0)
         progressBar.barBackgroundColor = UIColor(red:0.77, green:0.93, blue:0.78, alpha:1.0)
         progressBar.barBorderWidth = 1
-        progressBar.barFillInset = 3
+        progressBar.barFillInset = 2
         progressBar.labelTextColor = UIColor(red:0.35, green:0.80, blue:0.36, alpha:1.0)
-        progressBar.progressLabelInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+        progressBar.progressLabelInsets = labelInsets
+        
+        orangeProgressBar.progressLabelInsets = labelInsets
+        redProgressBar.progressLabelInsets = labelInsets
+        zeroProgressBar.progressLabelInsets = labelInsets
+    
     }
 
     override func didReceiveMemoryWarning() {
