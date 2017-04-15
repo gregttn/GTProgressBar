2951410cd313d5d9d966b4fe294f4ab6eddcf3cf
gregttn <gregttn@gmail.com>
2017-04-15 20:41:06 +0100

    Added enum with allowed positions for progress label.

---
+++ GTProgressBar/Classes/GTProgressBarLabelPosition.swift
@@ -0,0 +1,14 @@
+//
+//  GTProgressBarLabelPosition.swift
+//  Pods
+//
+//  Created by Grzegorz Tatarzyn on 15/04/2017.
+//
+//
+
+import Foundation
+
+public enum GTProgressBarLabelPostion {
+    case left
+    case right
+}
