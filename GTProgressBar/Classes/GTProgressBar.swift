f620b7ba9d8bf0e98e6c5b9595997568c10070d4
greg <gregttn@gmail.com>
2017-08-07 11:48:41 +0100

    Calculate corner radius on the correct dimension based on the orientation.

--- GTProgressBar/Classes/GTProgressBar.swift
+++ GTProgressBar/Classes/GTProgressBar.swift
@@ -277,6 +277,11 @@ public class GTProgressBar: UIView {
             return cornerRadius
         }
         
-        return min(view.frame.width, view.frame.height) / 2 * 0.7
+        switch orientation {
+        case .horizontal:
+            return view.frame.height / 2 * 0.7
+        case .vertical:
+            return view.frame.width / 2 * 0.7
+        }
     }
 }
