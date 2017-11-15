35b36ce1e6acb4eea3743efb313cdb0b2e60a4bb
greg <gregttn@gmail.com>
2017-11-15 09:10:39 +0000

    Added GTProgressBarDirection

---
+++ GTProgressBarDirection.swift
@@ -0,0 +1,12 @@
+//
+//  GTProgressBarDirection.swift
+//  GTProgressBar
+//
+//  Created by greg on 15/11/2017.
+//
+
+import Foundation
+
+public enum GTProgressBarDirection: Int {
+    case anticlocwise
+}
