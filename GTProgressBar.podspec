3f95061d042f25d15404918cb761b50ea01be081
Adar Porat <adar.porat@gmail.com>
2018-09-21 18:50:33 -0500

    support for both swift 4.0 & swift 4.2

--- GTProgressBar.podspec
+++ GTProgressBar.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'GTProgressBar'
-  s.version          = '0.3.0'
+  s.version          = '0.3.2'
   s.summary          = 'GTProgressBar is a customisable progress bar written in Swift 4'
 
 # This description is used to generate tags and improve search results.
--- GTProgressBar/Classes/UILabelExtensions.swift
+++ GTProgressBar/Classes/UILabelExtensions.swift
@@ -11,8 +11,13 @@ import Foundation
 internal extension UILabel {
     internal static func sizeFor(content: NSString, font: UIFont) -> CGSize {
         let text: NSString = content
+      
+        #if swift(>=4.2)
+        let textSize = text.size(withAttributes: [NSAttributedString.Key.font : font])
+        #else
         let textSize = text.size(withAttributes: [NSAttributedStringKey.font : font])
-        
+        #endif
+      
         return CGSize(width: ceil(textSize.width), height: ceil(textSize.height))
     }
 }
