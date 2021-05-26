0bd97c02d943fd157b544c956fa4105dcf23c6bc
MussaCharles <mussacharles50@gmail.com>
2021-05-26 13:45:05 +0900

    Add ability to pass custom animation duration

--- GTProgressBar/Classes/GTProgressBar.swift
+++ GTProgressBar/Classes/GTProgressBar.swift
@@ -298,7 +298,7 @@ public class GTProgressBar: UIView {
         progressLabel.text = "\(Int(round(_progress * 100)))%"
     }
     
-    public func animateTo(progress: CGFloat, completion: (() -> Void)? = nil) {
+    public func animateTo(progress: CGFloat,duration:TimeInterval = 0.8, completion: (() -> Void)? = nil) {
         let newProgress = min(max(progress,0), 1)
         let fillViewFrame = createFrameCalculator().fillViewFrameFor(progress: newProgress)
         let frameChange: () -> () = {
@@ -308,7 +308,7 @@ public class GTProgressBar: UIView {
         }
         
         if #available(iOS 10.0, *) {
-            let animation = UIViewPropertyAnimator(duration: 0.8, curve: .easeInOut, animations: frameChange)
+            let animation = UIViewPropertyAnimator(duration: duration, curve: .easeInOut, animations: frameChange)
             animation.addCompletion { (position) in
                 completion?()
             }
@@ -320,7 +320,7 @@ public class GTProgressBar: UIView {
             let animationOptions = UIViewAnimationOptions.curveEaseInOut
             #endif
 
-            UIView.animate(withDuration: 0.8,
+            UIView.animate(withDuration: duration,
                 delay: 0,
                 options: [animationOptions],
                 animations: frameChange,
